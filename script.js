cc45932550b519e9321353d745f3b145b5584e02
Neukirch <neukirchm@cppasd.com>
2017-11-27 14:07:38 -0500

    Allowed window to resize to client browser size

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function run()
 {
-	var c = document.getElementById("canvas");
-	var ctx = c.getContext("2d");
+	var c = document.getElementById("canvas"); //Get canvas
 
-	ctx.moveTo(0, 0);
-	ctx.lineTo(200, 100);
+	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; //Cross-browser way to get window size
+	var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+	c.width = width; //Set canvas size
+	c.height = height;
+
+	var ctx = c.getContext("2d"); //Get canvas to draw stuff
+
+	ctx.moveTo(0, 0); //Draw test line
+	ctx.lineTo(width, height);
+	ctx.moveTo(0, height);
+	ctx.lineTo(width, 0);
 	ctx.stroke();
 }
